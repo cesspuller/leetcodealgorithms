198acc374dc60ce9771c5d49efc0b11030795611
cesspuller <m3dusssa@gmail.com>
2022-01-13 19:50:02 +0300

    Добавил решение для пятой задачи блока вектор

---
+++ LeetCodeSolutions/LeetCodeSolutions/singleNumber.hpp
@@ -0,0 +1,24 @@
+#ifndef _T_single_Number_HPP_
+#define _T_single_Number_HPP_
+
+#include <vector>
+
+using namespace std;
+
+//! ƒл€ загрузки на лит код помен€ть название класса на Solution
+class TSingleNumber
+{
+   public:
+   int singleNumber( vector<int>&nums )
+   {
+      int ans = 0;
+
+      for ( auto res : nums )
+         ans = ans ^ res;
+
+      return ans;
+   }
+};
+
+#endif // _T_single_Number_HPP_
+
