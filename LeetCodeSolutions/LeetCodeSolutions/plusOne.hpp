f85bf56c91cbabafe6ea59350d06f3eb2a8cde4b
cesspuller <m3dusssa@gmail.com>
2022-01-13 20:06:44 +0300

    Добавил решение для седьмой задачи из блока вектор

---
+++ LeetCodeSolutions/LeetCodeSolutions/plusOne.hpp
@@ -0,0 +1,49 @@
+#ifndef _T_plus_One_HPP_
+#define _T_plus_One_HPP_
+
+#include <vector>
+
+using namespace std;
+
+//! Для загрузки на лит код поменять название класса на Solution
+class Solution
+{
+   public:
+
+      void recurs( vector<int>& digits, int k )
+      {
+
+         if ( ( digits[digits.size() - 1 - k] += 1 ) == 10 )
+         {
+            if ( k == ( digits.size() - 1 ) )
+            {
+               digits[digits.size() - 1 - k] = 1;
+
+               digits.resize( digits.size() + 1 );
+
+               return;
+            }
+
+            digits[digits.size() - 1 - k] = 0;
+
+
+            recurs( digits, k + 1 );
+         }
+         else
+            return;
+      }
+
+      vector<int> plusOne( vector<int>& digits )
+      {
+         int k = 0;
+
+         recurs( digits, k );
+
+         return digits;
+      };
+};
+
+#endif // _T_plus_One_HPP_
+
+
+
