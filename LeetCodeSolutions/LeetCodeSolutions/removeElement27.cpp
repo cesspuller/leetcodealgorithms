94efb8027f88778ef197c93008551cc00e3e0f71
TIsearcher <60257476+TIsearcher@users.noreply.github.com>
2023-11-27 01:02:47 +0300

    + задачка

---
+++ LeetCodeSolutions/LeetCodeSolutions/removeElement27.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+class Solution27
+{
+   public:
+   int removeElement( std::vector<int>& nums, int val )
+   {
+      int index = 0;
+
+      for ( int i = 0; i < nums.size(); i++ )
+      {
+         if ( nums[i] != val )
+         {
+            nums[index] = nums[i];
+            index++;
+         }
+      }
+
+      return index;
+   }
+};
